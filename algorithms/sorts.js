3d5db1a3336942da45c8ce58f0f6cab21f7aa96a
David Ladowitz <david@ladowitz.com>
2016-06-15 15:21:56 -0700

    getting slide function to work

--- algorithms/sorts.js
+++ algorithms/sorts.js
@@ -1,8 +1,19 @@
-var array = [2, 5, 8, 4, 78, 13, 33, 29, 7, 11]
+var array = [2, 5, 8, 10, 4, 78, 13, 33, 29, 7, 11]
 
-var slideToSort = function(array, index){
+// currentKey = 4
+// currentArray = [2, 4, 5, 8, 10, 78, 13, 33, 29, 7, 11]
+// currentIndex = 0
+var slideToSort = function(array, indexToMove){
+  var keyValue = array[indexToMove];
 
+  for(var index = indexToMove; index > 0; index--){
+    if(keyValue < array[index - 1]){
+      array[index] = array[index - 1];
+    } else {
+      array[index] = keyValue;
+    }
+  }
   return array;
 }
 
-console.log(slideToSort(array, 3))
+console.log(slideToSort(array, 4))
