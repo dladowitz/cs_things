330bb1350f273823a170aa1f1f71830ebc0320db
David Ladowitz <david@ladowitz.com>
2016-06-21 09:55:11 -0700

    updating

--- algorithms/hanoi.rb
+++ algorithms/hanoi.rb
@@ -4,11 +4,11 @@ def steps_to_move_disks(disks)
   elsif disks == 2
     return 3
   else
-    (2 * steps_to_move_disks(disks - 1) ) + 1
+    return (2 * steps_to_move_disks(disks - 1) ) + 1
   end
 end
 
 puts steps_to_move_disks(2)
 puts steps_to_move_disks(3)
 puts steps_to_move_disks(4)
-puts steps_to_move_disks(64)
+puts steps_to_move_disks(64) / 31536000
