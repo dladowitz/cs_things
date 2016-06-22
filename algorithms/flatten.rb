6753535aa734df0831224eb4ce2b1992323826f9
David Ladowitz <david@ladowitz.com>
2016-06-21 22:35:12 -0700

    flattened

--- algorithms/flatten.rb
+++ algorithms/flatten.rb
@@ -1,3 +1,40 @@
-def flatten
+require 'pry'
 
+# def flatten(array, flattened = [])
+#   array.each do |element|
+#     if element.is_a? Array
+#       flattened = flatten(element, flattened)
+#     else
+#       flattened << element
+#     end
+#   end
+#
+#   return flattened
+# end
+#
+# def flattify(array)
+#   array.each_with_object([]) do |element, flattened|
+#     flattened.push *(element.is_a?(Array) ? flattify(element) : element)
+#   end
+# end
+#
+# while array.any? { |e| e.is_a?(Array) } do
+#   array = array.each_with_object([]) do |element, flattened|
+#     element.is_a?(Array) ? element.inject(flattened,&:<<) : flattened << element
+#   end;
+# end;
+
+
+def flattify(array, flattened = [])
+  array.each do |element|
+    if element.is_a? Array
+      flattened = flattify(element, flattened)
+    else
+      flattened << element
+    end
+  end
+  return flattened
 end
+a = ["A", "A", "B", ["A", "B"], "A", ["A", ["A"]], "C"]
+
+p flattify(a)
